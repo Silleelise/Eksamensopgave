fdaa94673ce5125a0856a4207b56e350e3c83912
Sille Elise Lindskow Hansen <silleeliselindskowhansen@Silles-MacBook-Air.local>
2020-11-19 17:45:20 +0100

    test

---
+++ client/model/user.js
@@ -0,0 +1,19 @@
+class user {
+  constructor(
+    firstName,
+    lastName,
+    birthDate,
+    gender,
+    aboutMe,
+    email,
+    password
+  ) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.birthDate = birthDate;
+    this.gender = gender;
+    this.aboutMe = aboutMe;
+    this.email = email;
+    this.password = passwor;
+  }
+}
