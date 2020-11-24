fc35816d6811ae070af1d4333bc76b3cba2bb361
Sille Elise Lindskow Hansen <silleeliselindskowhansen@Silles-MacBook-Air.local>
2020-11-24 17:16:16 +0100

    Login controller på email og password html form

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const express = require("express"); //Importerer express pakke
 const app = express(); //eksikverer express//Gemmer
 const PORT = process.env.port || 4000; //sætter serveren til at lytte på port 4000
-const user = require('../client/model/user.js"'); // henter infomation fra klassen match API
+const userClass = require('../client/model/user.js"'); // henter infomation fra klassen match API
+
+users = [];
 
 //Routes CRUD
 app.get("/user", (req, res) => {
@@ -16,8 +18,27 @@ app.put("/user", (req, res) => {
   res.json(user, "User has been updated");
 });
 
-app.delete("/user", (req, res) => {
-  res.json(user, "User has been deleted");
+app.delete("/user", (req, res) => { //sletter en bruger hvor en email matcher
+  for (index = 0; index < users.length; ++index) { //For loop: går igennem hver bruger og finder email der matcher
+    if (users[index].email==req.body.email){ 
+      users.splice(index, 1) //hvis der er en email der matcher, vil den slette brugeren
+  } res.send("delete") //fortæller at brugeren er slettet 
+});
+
+
+//Sign up
+app.post("/signup", (req, res) => { //opretter en user med attributter fra requestet
+  let newUser = new userClass.user(req.body.firstName, req.body.lastName, req.body.birthDate, req.body.gender, req.body.aboutMe, req.body.email, req.body.password)
+  users.push(newUser); //tilføjer brugerne til users array
+  res.send("User created") //fortæller at brugeren er oprettet 
+});
+
+//Login 
+app.post("/login", (req, res) => { 
+  for (index = 0; index < users.length; ++index) { //Går igennem hver bruger og finder email og password der matcher 
+    if (users[index].email==req.body.email){ 
+      if (users[index].password==req.body.password){ 
+    }
 });
 
 // Severen lytter på porten
