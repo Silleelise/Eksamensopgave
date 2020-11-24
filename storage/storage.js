51f9cd52a38bbbe9f0e3b98e2551714562df5ac0
Sille Elise Lindskow Hansen <silleeliselindskowhansen@Silles-MacBook-Air.local>
2020-11-24 15:59:25 +0100

    local storage

---
+++ storage/storage.js
@@ -0,0 +1,11 @@
+// opstiller variable for hver sign up form og giver dem et kaldenavn
+var name1 = document.getElementById("name1");
+var pw = document.getElementById("pw");
+var email = document.getElementById("email");
+var birthday = document.getElementById("birthday");
+var genderFemale = document.getElementById("female");
+var genderMale = document.getElementById("male");
+var genderOther = document.getElementById("other");
+var interestFemale = document.getElementById("Female");
+var interestMale = document.getElementById("Male");
+var interestOther = document.getElementById("Other");
