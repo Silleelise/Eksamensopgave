6346b2234abd95b56e48863e83ff63e402aaf736
Sille Elise Lindskow Hansen <silleeliselindskowhansen@Silles-MacBook-Air.local>
2020-11-24 16:38:47 +0100

    kalder local storage

--- storage/storage.js
+++ storage/storage.js
@@ -9,3 +9,35 @@ var genderOther = document.getElementById("other");
 var interestFemale = document.getElementById("Female");
 var interestMale = document.getElementById("Male");
 var interestOther = document.getElementById("Other");
+
+// kalder localstorage og bruger set funktionen til at sætte keyword til den value
+function store() {
+  var gender = null;
+
+  if (document.getElementById("male").checked) {
+    gender = document.getElementById("male").value;
+  }
+  if (document.getElementById("female").checked) {
+    gender = document.getElementById("female").value;
+  }
+  if (document.getElementById("other").checked) {
+    gender = document.getElementById("other").value;
+  }
+
+  var interest = null;
+  if (document.getElementById("Male").checked) {
+    interest = document.getElementById("Male").value;
+  }
+  if (document.getElementById("Female").checked) {
+    interest = document.getElementById("Female").value;
+  }
+  if (document.getElementById("Other").checked) {
+    interest = document.getElementById("Other").value;
+  }
+  localStorage.setItem("name1", name1.value);
+  localStorage.setItem("pw", pw.value);
+  localStorage.setItem("email", email.value);
+  localStorage.setItem("birthday", birthday.value);
+  localStorage.setItem("gender", gender);
+  localStorage.setItem("interest", interest);
+}
